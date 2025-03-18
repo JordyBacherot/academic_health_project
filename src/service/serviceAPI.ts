40a5bd592a9f78ceb553aef3c3fdb87c5609cc0f
JordyBacherot <jordy.bacherot@outlook.fr>
2025-03-18 22:56:48 +0100

    Ajout de fonction d'appel sur l'API

--- src/service/serviceAPI.ts
+++ src/service/serviceAPI.ts
@@ -1,13 +1,33 @@
 import axios from "axios";
 
-async function call_items_people() {
-    try {
-        // Envoi d'une requête GET vers une API d'exemple
-        const response = await axios.get("https://health.shrp.dev/items/people");
-        console.log("Données récupérées :", response.data);
-    } catch (error) {
-        console.error("Erreur lors de la requête :", error);
+// With access_token
+
+// Without access_token
+export async function call_items_people(id: string = "") {
+    if (id !== "") {
+        return call_API("https://health.shrp.dev", `items/people/${id}`);
+    }
+    else {
+        return call_API("https://health.shrp.dev", "items/people");
     }
 }
 
-call_items_people();
+export async function call_items_physiologicalData() {
+    return await call_API("https://health.shrp.dev", "items/physiologicalData");
+}
+
+export async function call_items_physicalActivities() {
+    return call_API("https://health.shrp.dev", "items/physicalActivities");
+}
+
+// Intern function to avoid code duplication
+async function call_API(url: string, endpoint: string) {
+    let response = null;
+    try {
+        response = await axios.get(`${url}/${endpoint}`);
+    } catch (error) {
+        console.error(`Request Error (${endpoint}): ${error}`);
+        return ;
+    }
+    return response.data
+}
