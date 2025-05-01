0e749af4350c253158dd9c1ee4634427ae8fcd1d
JordyBacherot <jordy.bacherot@outlook.fr>
2025-05-01 13:59:56 +0200

    Clean App

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ function App() {
             <div className="App">
                 <Contexts>
                     <MenuPrincipal/>
-                    <Globalchat></Globalchat>
                 </Contexts>
             </div>
             <Background/>
