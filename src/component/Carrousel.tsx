2ffc8b3ee29839a8ddcd26d5607d9af8342c440c
Neirpy <48681341+Neirpy@users.noreply.github.com>
2025-05-10 11:38:38 +0200

    add: debut du style du carrousel
    Début du travail sur le carrousel notamment sur le fait de passer d'un patient à l'autre dans l'affichage

--- src/component/Carrousel.tsx
+++ src/component/Carrousel.tsx
@@ -1,25 +1,41 @@
 import {usePatients} from "../contexts/PatientsContext.ts";
+import {useRef} from "react";
 
 function Carrousel(){
  // todo : recupérer les infos des patients
+    const carrouselRef = useRef(null);
     const patients = usePatients()
+    console.log(patients[0])
 
+const scrollLeft = () => {
+    if (carrouselRef.current) {
+        const scrollAmount = carrouselRef.current.offsetWidth * 0.8; // 80% de la largeur visible
+        carrouselRef.current.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    }
+};
+
+const scrollRight = () => {
+    if (carrouselRef.current) {
+        const scrollAmount = carrouselRef.current.offsetWidth * 0.8; // 80% de la largeur visible
+        carrouselRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    }
+};
     return (
         <div className="carrousel">
-            <div className="carrousel__container">
-                <div className="carrousel__item">
-                    <img src="../public/background.png" alt="Patient 1"/>
-                    <p>Patient 1</p>
-                </div>
-                <div className="carrousel__item">
-                    <img src="../public/background.png" alt="Patient 2"/>
-                    <p>Patient 2</p>
-                </div>
-                <div className="carrousel__item">
-                    <img src="../public/background.png" alt="Patient 3"/>
-                    <p>Patient 3</p>
-                </div>
+            <button className="button_left_carrou" onClick={scrollLeft}>◀</button>
+            <div className="carrousel_container" ref={carrouselRef}>
+                {patients?.map((patient) => (
+                    <div key={patient.id} className="carrousel_item">
+                        <img
+                            src={patient.sex === 1 ? "user-masculin.svg" : "user-feminin.svg"}
+                            alt="Patient Icon"
+                        />
+                        <p>{patient.firstname} {patient.lastname}</p>
+                    </div>
+                    ))
+                }
             </div>
+            <button className="button_right_carrou" onClick={scrollRight}>▶</button>
         </div>
     )
 }
--- src/pages/PatientsMaster.tsx
+++ src/pages/PatientsMaster.tsx
@@ -3,7 +3,7 @@ import RenderPatients from "../component/RenderPatients.tsx";
 function PatientsMaster() {
 
     return (
-        <div>
+        <div className="detail_page">
             <RenderPatients/>
         </div>
     );
