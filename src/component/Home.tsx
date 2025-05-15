f4bec940e5b787a7f47421b1275a14d949379bc8
DevLog7 <loghannbdt@gmail.com>
2025-05-15 16:31:21 +0200

    update page accueil + ajout images

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -5,31 +5,46 @@ export default function Home(){
 
 
   const boxes = [
-    "Un système de suivi personnalisé pour chaque patient, vous permettant de consulter leurs informations, leurs données physiologiques, leurs données d'activité physique, et leurs données psychologiques à tout moment.",
-    "Un moteur de recherche vous aidant à trouver n'importe lequel de vos patients en quelques secondes et d'accéder directement à son profil",
+    "Un système de suivi personnalisé pour chaque patient, pourvu d'un moteur de recherche vous aidant à trouver n'importe lequel de vos patients en quelques secondes et d'accéder directement à son profil.",
+    "Un profil détaillé pour chaque patient, incluant leurs données physiologiques, leurs données d'activité physique, et leurs données psychologiques.",
     "Un emploi du temps interactif, vous assistant dans l'organisation de vos rendez-vous et vous permettant de consulter les disponibilités de vos patients. Vous pourrez observer les rendez-vous par mois, semaine, jour, ou sous forme d'agenda vous indiquant les rendez-vous à suivre.",
     "Un chat global, vous donnant l'occasion d'échanger des informations et questions avec vos confrères utilisant l'application, ainsi qu'un chat individuel avec chaque patient, vous permettant de répondre à leurs questions et de les rassurer.",
     "Un chatbot vous suggérant un programme d'entraînement physique pour un patient, en se basant sur les informations que vous lui fournirez.",
   ];
 
+  const images = [
+    "/icons/icon_health_home.svg",
+    "/icons/icon_graph.svg",
+    "/icons/icon_time.svg",
+    "/icons/icon_pen.svg",
+    "/icons/chatbot.svg"
+    ];
+
+    const altTexts = [
+        "a heart with pulse",
+        "a graph", 
+        "a calendar",
+        "a pen",
+        "a chatbot"
+    ];
+
 return (
     <>
     <div className = "text-xl font-weight : bold flex items-center text-center justify-center font-weight : bold ">
-        <img src="/icons/logo_small.svg" alt="medecin" className="mb-4 h-20 w-20 flex justify-center m-4 items-center absolute start-0"/>
-        Bienvenue sur HEALTH PROJECT
+        <img src="/icons/logo_small.svg" alt="medecin" className="mb-4 sm:w-10 sm:h-10 h-20 w-20 flex justify-center m-4 items-center start-0"/>
+        HEALTH PROJECT
     </div>
     <div className = "bg-white/10 w-full max-width : 700px p-6 rounded-lg shadow-lg flex flex-col items-center justify-center">
-        <p className = "text-center flex items-center justify-center">Sur cette application, vous trouverez :</p>
+        <p className = "text-center flex items-center justify-center">Une application où vous trouverez :</p>
     </div>
-    <div className="flex flex-col gap-6 p-6">
+    <div className="flex flex-col gap-6 p-6 item-center justify-center sm:flex-col sm:flex-wrap sm:gap-4">
     {boxes.map((text, index) => (
         <div
-            key={index}
-            className="justify-start"
-        >
-            <div className="bg-white/25 h-90 text-black shadow-lg p-4 rounded-lg w-2/3 max-w-md flex items-center justify-center text-center margin-2">
+            key={index}       >
+            <div className="bg-white/25 h-90 text-black shadow-lg p-4 rounded-lg  flex items-center justify-center text-center margin-2">
             {text}
             </div>
+            <img src={images[index]} alt={altTexts[index]} className="w-32 h-32 object-contain mb-4 opacity-50 flex items-center justify-center"/>
         </div>
     ))}
 </div>
